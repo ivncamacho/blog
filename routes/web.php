96cf91ca0856c70cae0b94a1a1a5a27ef9989403
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-04 17:26:11 +0200

    parte 6 corregida

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 Route::view('contacto', 'contact')->name('contact');
-Route::view('blog', function () {
+Route::get('blog', function () {
     $posts = [
         ['title' => 'Post 1'],
         ['title' => 'Post 2'],
