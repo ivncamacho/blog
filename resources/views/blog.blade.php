7a75d115563c4ff04631a575430572de9652dcba
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-04 17:11:04 +0200

    parte 6 (mitad)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-@include('partials.navigation')
-<h1>Blog</h1>
-</body>
-</html>
+<x-layout meta-title="Blog" meta-description="Descripcion de la pagina del blog">
+    <h1>Blog</h1>
+    @foreach($posts as $post)
+        <h2>{{ $post['title']}}</h2>
+    @endforeach
+</x-layout>
+
