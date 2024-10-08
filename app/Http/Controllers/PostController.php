87a82822026fdd550afd98357f67fb566e371976
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-08 16:04:08 +0200

    Controlador invocable

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,5 +4,14 @@ namespace App\Http\Controllers;
 
 class PostController
 {
+    public function __invoke()
+    {
+        $posts = [
+            ['title' => 'Post 1'],
+            ['title' => 'Post 2'],
+            ['title' => 'Post 3'],
+            ['title' => 'Post 4'],
+    ];
 
+}
 }
