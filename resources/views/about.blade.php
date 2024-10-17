bbcf0a43aef29ce0463171183b75da419770c199
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-17 15:50:07 +0200

    paso 26 cambio estilo de titulo de dentro de cada apartado

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,7 @@
 <x-layout meta-title="Contacto" meta-description="Descripcion de la pagina de contacto">
-    <h1>Nosotros</h1>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            About us
+        </h1>
+    </div>
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 <x-layout meta-title="Inicio" meta-description="Descripcion de la pagina de inicio">
-    <h1>Inicio</h1>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Welcome
+        </h1>
+    </div>
     <x-slot:sidebar>
         <p>Home sidebar</p>
     </x-slot:sidebar>
