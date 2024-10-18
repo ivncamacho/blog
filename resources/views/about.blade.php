6d8400a1a9f801ed834305c808a36f5dc554777d
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-18 16:31:55 +0200

    paso 30 ponemos el meta tilte y meta description correctamente

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-<x-layout meta-title="Contacto" meta-description="Descripcion de la pagina de contacto">
+<x-layout meta-title="Nosotros" meta-description="Descripcion de la pagina sobre nosotros">
     <div class="mx-auto mt-4 max-w-6xl">
         <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
             About us
