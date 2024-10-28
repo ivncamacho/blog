c776300f3cc2b1636f1d0745ece9908f75257c1f
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-10-28 17:41:21 +0100

    AJUSTE DE ICONO SUPERIOR DERECHO

--- resources/views/layouts/blog-navigation.blade.php
+++ resources/views/layouts/blog-navigation.blade.php
@@ -241,6 +241,7 @@
                     <button
                         id="toggle-session-menu"
                         class="ml-4 rounded-full text-slate-500 transition-colors hover:text-sky-500 focus:ring-2 focus:ring-slate-200 focus:ring-offset-1"
+                        style="position: relative; top: 4px;"
                     >
                         <img
                             class="h-6 w-6 rounded-full"
